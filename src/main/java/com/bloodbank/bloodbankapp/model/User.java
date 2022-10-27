aa541af460751a14fb29273199cf9e2729881347
Vladimir Lunić <vladimir.lunic2000@gmail.com>
2022-10-27 18:18:42 +0200

    Add User model

---
+++ src/main/java/com/bloodbank/bloodbankapp/model/User.java
@@ -0,0 +1,21 @@
+package com.bloodbank.bloodbankapp.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+
+    private String email;
+    private String password;
+    private String firstName;
+    private String lastName;
+    private String JMBG;
+    private String phoneNumber;
+    private String occupation;
+    private boolean active;
+    private int penalties;
+}
