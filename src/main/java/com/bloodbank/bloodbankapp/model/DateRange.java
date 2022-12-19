5f3d9bfb322c5ed4700f853be4e83174827f7f97
Vladimir Lunić <vladimir.lunic2000@gmail.com>
2022-12-19 10:02:07 +0100

    Add handling of dates and ranges before, during and after a given date range

--- src/main/java/com/bloodbank/bloodbankapp/model/DateRange.java
+++ src/main/java/com/bloodbank/bloodbankapp/model/DateRange.java
@@ -14,4 +14,11 @@ public class DateRange {
         return Duration.between(this.Start, this.End);
     }
 
+    public boolean dateIsBefore(LocalDateTime other) { return other.isBefore(this.Start); }
+
+    public boolean dateIsAfter(LocalDateTime other) { return other.isAfter(this.End); }
+
+    public boolean dateIsDuring(LocalDateTime other) { return other.isAfter(this.Start) && other.isBefore(this.End); }
+
+    public boolean rangeIsDuring(DateRange other) { return other.Start.isAfter(this.Start) && other.End.isBefore(this.End); }
 }
