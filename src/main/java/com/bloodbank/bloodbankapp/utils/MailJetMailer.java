a7c4fc5c5dab0c5d89cdf76bcc6d4afc5b3cae29
Gravarica <93195018+Gravarica@users.noreply.github.com>
2022-12-21 23:23:45 +0100

    Finished spring security finally (#30)

--- src/main/java/com/bloodbank/bloodbankapp/utils/MailJetMailer.java
+++ src/main/java/com/bloodbank/bloodbankapp/utils/MailJetMailer.java
@@ -8,9 +8,9 @@ import com.mailjet.client.resource.Email;
 
 public class MailJetMailer {
 
+    private final static String PublicMailjetKey = "6cdf2011e792898a6181e4e0d8c93b0d";
+    private final static String PrivateMailjetKey = "188cd0c4f83511584789aaa4804d6d35";
     public static void SendScheduleAppointmentMail(String recipient) throws MailjetException {
-        String PublicMailjetKey = "6cdf2011e792898a6181e4e0d8c93b0d";
-        String PrivateMailjetKey = "188cd0c4f83511584789aaa4804d6d35";
 
         String template = "<mjml>" +
                 "<mj-body>" +
@@ -29,7 +29,36 @@ public class MailJetMailer {
         MailjetResponse response;
         client = new MailjetClient(PublicMailjetKey, PrivateMailjetKey);
         request = new MailjetRequest(Email.resource)
-                .property(Email.FROMEMAIL, "pswhospital@google.com")
+                .property(Email.FROMEMAIL, "psw.hospital.2022@gmail.com")
+                .property(Email.FROMNAME, "Blood Bank Team")
+                .property(Email.SUBJECT, "Successful Appointment Scheduling")
+                .property(Email.HTMLPART, template)
+                .property(Email.TO, recipient);
+        response = client.post(request);
+        System.out.println(response.getStatus());
+        System.out.println(response.getData());
+    }
+
+    public static void SendActivateAccountMail(String recipient) throws MailjetException {
+
+        String template = "<mjml>" +
+                "<mj-body>" +
+                "<mj-section background-color=\"#ffffff\" padding-top=\"0\">" +
+                "<mj-column width=\"500px\">" +
+                "<mj-text font-size=\"16px\" align=\"left\">" +
+                "<p>Please activate your account by pressing this <a href=" + "'http://localhost:3000/activate/" + recipient + "'" + ">link<a/> </p>" +
+                "</mj-text>" +
+                "</mj-column>" +
+                "</mj-section>" +
+                "</mj-body>" +
+                "</mjml>";
+
+        MailjetClient client;
+        MailjetRequest request;
+        MailjetResponse response;
+        client = new MailjetClient(PublicMailjetKey, PrivateMailjetKey);
+        request = new MailjetRequest(Email.resource)
+                .property(Email.FROMEMAIL, "psw.hospital.2022@gmail.com")
                 .property(Email.FROMNAME, "Blood Bank Team")
                 .property(Email.SUBJECT, "Successful Appointment Scheduling")
                 .property(Email.HTMLPART, template)
