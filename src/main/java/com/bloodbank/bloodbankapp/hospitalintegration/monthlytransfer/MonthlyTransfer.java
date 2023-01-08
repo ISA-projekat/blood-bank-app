b490b6ec066c716730c1f1ef0992ba4ff473ccaf
Vladimir Lunić <vladimir.lunic2000@gmail.com>
2023-01-08 16:18:19 +0100

    Add monthly transfers

---
+++ src/main/java/com/bloodbank/bloodbankapp/hospitalintegration/monthlytransfer/MonthlyTransfer.java
@@ -0,0 +1,23 @@
+package com.bloodbank.bloodbankapp.hospitalintegration.monthlytransfer;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+@Getter
+@Setter
+public class MonthlyTransfer {
+    public LocalDate DateTime;
+    public int APlus;
+    public int AMinus;
+    public int BPlus;
+    public int BMinus;
+    public int ABPlus;
+    public int ABMinus;
+    public int OPlus;
+    public int OMinus;
+
+    public MonthlyTransfer(){}
+
+
+}
