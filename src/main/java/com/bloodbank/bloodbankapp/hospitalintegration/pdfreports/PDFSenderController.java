76b3979ae2933c4ec9e75ca992c0a49a29f43541
Vladimir Lunić <vladimir.lunic2000@gmail.com>
2023-01-08 16:20:35 +0100

    Add pdf reports

---
+++ src/main/java/com/bloodbank/bloodbankapp/hospitalintegration/pdfreports/PDFSenderController.java
@@ -0,0 +1,26 @@
+package com.bloodbank.bloodbankapp.hospitalintegration.pdfreports;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+
+@RestController
+@RequestMapping("/pdf-sender")
+@RequiredArgsConstructor
+public class PDFSenderController {
+
+    @CrossOrigin
+    @PostMapping("/send")
+    public ResponseEntity sendPdf(@RequestBody MultipartFile pdf) throws Exception{
+        // Used for sending PDFs of monthly reports
+
+        String filePath = "..\\PDF\\"+pdf.getOriginalFilename();
+        Files.copy(pdf.getInputStream(), Paths.get(filePath), StandardCopyOption.REPLACE_EXISTING);
+        return ResponseEntity.ok("Sent");
+    }
+}
