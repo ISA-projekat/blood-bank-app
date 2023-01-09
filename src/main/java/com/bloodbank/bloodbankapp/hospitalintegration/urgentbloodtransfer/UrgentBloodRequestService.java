dd13a737ff88666a5cbfdef8fb5131fd9111c683
Vladimir Lunić <vladimir.lunic2000@gmail.com>
2023-01-09 10:10:24 +0100

    Add Urgent blood transfer service

--- src/main/java/com/bloodbank/bloodbankapp/hospitalintegration/urgentbloodtransfer/UrgentBloodRequestService.java
+++ src/main/java/com/bloodbank/bloodbankapp/hospitalintegration/urgentbloodtransfer/UrgentBloodRequestService.java
@@ -1,5 +1,6 @@
 package com.bloodbank.bloodbankapp.hospitalintegration.urgentbloodtransfer;
 
+import com.bloodbank.bloodbankapp.hospitalintegration.bloodinquiry.BloodInquiryService;
 import grpcService.Model;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -7,10 +8,10 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class UrgentBloodRequestService {
-    // fill class with methods you need for the controller to work
+
+    private final BloodInquiryService bloodInquiryService;
 
     public boolean hasBlood(Model.BloodType bloodType, int amount) {
-
-        return true;
+        return bloodInquiryService.hasBloodTypeAmount(bloodType.name(), amount);
     }
 }
